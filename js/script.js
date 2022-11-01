639ee26b6a6891b8ba91c9312ac4ce74a196dc07
Joshua Andreas <josh@Joshuas-MacBook-Pro.local>
2022-11-01 16:05:56 -0600

    pseudo & js work

---
+++ js/script.js
@@ -0,0 +1,55 @@
+/*-----------constants----------*/
+const superCombo = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+
+
+
+
+
+/*----- app's state (variables) -----*/
+const board = 
+let turn = 
+let winner = 
+
+/*----- cached element references -----*/
+
+
+
+
+
+
+
+
+
+/*----- event listeners -----*/
+//click 'reset'
+
+//click 'tile'
+
+/*----- functions -----*/
+//reset
+initialize();
+
+//handle click
+function handleClick(){
+    
+}
+
+//render
+function render(){
+    if (winner){
+        renderWinMsg(`Winner is ${}`)
+    }
+}
+
+function initialize(){
+
+}
