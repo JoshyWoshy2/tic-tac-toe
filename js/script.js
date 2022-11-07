ec0219b37da5d7d668c614a3b4f0522a308406a0
Joshua Andreas <josh@Joshuas-MacBook-Pro.local>
2022-11-07 08:44:25 -0700

    Complete ttt

--- js/script.js
+++ js/script.js
@@ -1,55 +1,39 @@
 /*-----------constants----------*/
-const superCombo = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-]
+// const superCombo = [
+//     [0,1,2],
+//     [3,4,5],
+//     [6,7,8],
+//     [0,3,6],
+//     [1,4,7],
+//     [2,5,8],
+//     [0,4,8],
+//     [2,4,6]
+// ]
 
-
-
-
-
-/*----- app's state (variables) -----*/
-const board = 
-let turn = 
-let winner = 
-
-/*----- cached element references -----*/
-
-
-
-
-
-
-
-
+const board = document.getElementsByClassName("square");
 
 /*----- event listeners -----*/
-//click 'reset'
-
-//click 'tile'
-
+document.addEventListener("click", handleClick);
+let squareClick = 0;
 /*----- functions -----*/
 //reset
-initialize();
 
 //handle click
-function handleClick(){
-    
-}
-
-//render
-function render(){
-    if (winner){
-        renderWinMsg(`Winner is ${}`)
+function handleClick(event) {
+  if (event.target.classList.contains("square")) {
+    squareClick++;
+    // if (squareClick > 9){
+    //     initialize()
+    // } else
+    if (squareClick % 2) {
+      event.target.innerHTML = "O";
+    } else {
+      event.target.innerHTML = "X";
     }
-}
-
-function initialize(){
-
+  } else if (event.target.classList.contains("fa-solid")) {
+    const boardArr = [...board];
+    boardArr.forEach((square) => (square.innerHTML = ""));
+    //  console.log('I should work')
+  }
+  console.log();
 }
